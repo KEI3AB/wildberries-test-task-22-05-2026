822bdcb236930178c2b84dd8d3324ef511b696a6
KEI3AB <ramazanoff06@yandex.ru>
2026-05-23 13:33:58 +0300

    feat: implement trend usecase with stop-list validation and TTL check

--- internal/usecase/trend.go
+++ internal/usecase/trend.go
@@ -2,6 +2,8 @@ package usecase
 
 import (
 	"context"
+	"log/slog"
+	"time"
 
 	"github.com/wildberries-test-task-22-05-2026/internal/domain"
 )
@@ -13,6 +15,37 @@ type TrendRepository interface {
 	GetTopNTrends(ctx context.Context, n int) ([]domain.TrendQuery, error)
 }
 
+type StopListChecker interface {
+	IsBlocked(ctx context.Context, query string) (bool, error)
+}
+
 type TrendManager struct {
-	repo TrendRepository
+	repo    TrendRepository
+	checker StopListChecker
+}
+
+func NewTrendManager(repo TrendRepository, checker StopListChecker) *TrendManager {
+	return &TrendManager{
+		repo:    repo,
+		checker: checker,
+	}
+}
+
+func (uc *TrendManager) AddSearchEvent(ctx context.Context, event domain.SearchEvent) error {
+	if ok, err := uc.checker.IsBlocked(ctx, event.NormalizedQuery); ok {
+		return nil
+	} else if err != nil {
+		slog.Error("stop list error", "err", err)
+	}
+
+	currTime := time.Now().UnixMilli()
+	if (currTime-event.Timestamp)/60000 > 5 {
+		return nil // Запрос старше 5 минут
+	}
+
+	return uc.repo.AddSearchEvent(ctx, event)
+}
+
+func (uc *TrendManager) GetTopNTrends(ctx context.Context, n int) ([]domain.TrendQuery, error) {
+	return uc.repo.GetTopNTrends(ctx, n)
 }
