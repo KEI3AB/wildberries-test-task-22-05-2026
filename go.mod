9fd4ff7487522297a1994b4a748f427c2bd2d8d8
KEI3AB <ramazanoff06@yandex.ru>
2026-05-23 22:43:32 +0300

    feat: implement kafka consumer with easyjson and franz-go

--- go.mod
+++ go.mod
@@ -2,6 +2,16 @@ module github.com/wildberries-test-task-22-05-2026
 
 go 1.26.1
 
-require github.com/redis/rueidis v1.0.75
+require (
+	github.com/mailru/easyjson v0.9.2
+	github.com/redis/rueidis v1.0.75
+	github.com/twmb/franz-go v1.21.2
+)
 
-require golang.org/x/sys v0.43.0 // indirect
+require (
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/klauspost/compress v1.18.6 // indirect
+	github.com/pierrec/lz4/v4 v4.1.26 // indirect
+	github.com/twmb/franz-go/pkg/kmsg v1.13.1 // indirect
+	golang.org/x/sys v0.43.0 // indirect
+)
---
+++ internal/transport/kafka/consumer.go
@@ -0,0 +1,73 @@
+package kafka
+
+//go:generate easyjson $GOFILE
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/mailru/easyjson"
+	"github.com/twmb/franz-go/pkg/kgo"
+	"github.com/wildberries-test-task-22-05-2026/internal/domain"
+)
+
+//easyjson:json
+type SearchEventPayload struct {
+	SessionID       string `json:"session_id"`
+	Timestamp       int64  `json:"timestamp"`
+	NormalizedQuery string `json:"normalized_query"`
+}
+
+type TrendUseCase interface {
+	AddSearchEvent(ctx context.Context, event domain.SearchEvent) error
+}
+
+type Consumer struct {
+	client  *kgo.Client
+	trendUC TrendUseCase
+}
+
+func NewConsumer(kafkaClient *kgo.Client, uc TrendUseCase) *Consumer {
+	return &Consumer{
+		client:  kafkaClient,
+		trendUC: uc,
+	}
+}
+
+func (tr *Consumer) Start(ctx context.Context) {
+	for {
+		fetches := tr.client.PollFetches(ctx)
+		if err := fetches.Err(); err != nil {
+			if ctx.Err() != nil {
+				break
+			}
+			slog.Error("batch fetching error", "err", err)
+			continue
+		}
+
+		iter := fetches.RecordIter()
+		for !iter.Done() {
+			record := iter.Next()
+
+			var dto SearchEventPayload
+			if err := easyjson.Unmarshal(record.Value, &dto); err != nil {
+				slog.Error("kafka DTO easyjson unmarshalling error", "err", err)
+				continue
+			}
+
+			err := tr.trendUC.AddSearchEvent(ctx, searchEventPayloadDTOToSearchEventDomain(dto))
+			if err != nil {
+				slog.Error("failed to process event", "err", err)
+			}
+		}
+	}
+}
+
+// мапер DTO в доменную структуру
+func searchEventPayloadDTOToSearchEventDomain(dto SearchEventPayload) domain.SearchEvent {
+	return domain.SearchEvent{
+		SessionID:       dto.SessionID,
+		Timestamp:       dto.Timestamp,
+		NormalizedQuery: dto.NormalizedQuery,
+	}
+}
