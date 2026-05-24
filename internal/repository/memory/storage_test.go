2bd4a3ab929905f853adecfa0c2ab38eeb5d170c
KEI3AB <ramazanoff06@yandex.ru>
2026-05-24 11:11:07 +0300

    test: add benchmarks for ring buffer storage

--- internal/repository/memory/storage_test.go
+++ internal/repository/memory/storage_test.go
@@ -2,6 +2,8 @@ package memory
 
 import (
 	"context"
+	"fmt"
+	"math/rand"
 	"sync"
 	"testing"
 	"time"
@@ -134,4 +136,67 @@ func TestRingBuffer_Concurrency(t *testing.T) {
 	require.Len(t, result, 1)
 	assert.Equal(t, "iphone", result[0].Query)
 	assert.Equal(t, goroutinesCount, result[0].NumOfReq)
+}
+
+// Бенчмаркаем последовательное добавление
+func BenchmarkRingBuffer_AddSearchEvent(b *testing.B) {
+	buffer := NewRingBuffer()
+	ctx := context.Background()
+	baseTime := time.Now().UnixMilli()
+
+	events := make([]domain.SearchEvent, b.N)
+	for i := 0; i < b.N; i++ {
+		events[i] = domain.SearchEvent{
+			SessionID:       fmt.Sprintf("session_id%d", i),
+			Timestamp:       baseTime,
+			NormalizedQuery: fmt.Sprintf("query_%d", i),
+		}
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = buffer.AddSearchEvent(ctx, events[i])
+	}
+}
+
+// Бенчмарк параллельного добавления
+func BenchmarkRingBuffer_AddSearchEvent_Parallel(b *testing.B) {
+	buffer := NewRingBuffer()
+	ctx := context.Background()
+	baseTime := time.Now().UnixMilli()
+
+	b.ResetTimer()
+	b.RunParallel(func(p *testing.PB) {
+		i := rand.Intn(25_000_000)
+		for p.Next() {
+			ev := domain.SearchEvent{
+				SessionID:       fmt.Sprintf("session_%d", i),
+				Timestamp:       baseTime,
+				NormalizedQuery: fmt.Sprintf("query_%d", i%1000),
+			}
+			_ = buffer.AddSearchEvent(ctx, ev)
+			i++
+		}
+	})
+}
+
+// Бенчмарк получения топа
+func BenchmarkRingBuffer_GetTopNTrends(b *testing.B) {
+	buffer := NewRingBuffer()
+	ctx := context.Background()
+	baseTime := time.Now().UnixMilli()
+
+	for i := 0; i < 25_000_000; i++ {
+		ev := domain.SearchEvent{
+			SessionID:       fmt.Sprintf("session_%d", i),
+			Timestamp:       baseTime,
+			NormalizedQuery: fmt.Sprintf("query_%d", i%10000),
+		}
+		_ = buffer.AddSearchEvent(ctx, ev)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = buffer.GetTopNTrends(ctx, 10)
+	}
 }
